72649faa9ae755aeccca24023b5f748d2f9d2093
Kendyl Hull <hullkendyl@gmail.com>
2021-04-24 21:41:34 -0400

    Created the game board

--- src/main/java/TicTacToeApplication.java
+++ src/main/java/TicTacToeApplication.java
@@ -1,6 +1,20 @@
 public class TicTacToeApplication {
 
     public static void main(String[] args) {
-        
+
+        char [] [] gameBoard = {
+                {' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '}
+        };
+
+        for (char[] row : gameBoard) {
+            for(char c: row) {
+                System.out.print(c);
+            }
+            System.out.println();
+        }
     }
 }
